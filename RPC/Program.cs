236e065533caaf5716770e95fb6f9dfadacccfac
SijyKijy <max.monovcov@gmail.com>
2020-04-05 14:28:09 +0300

    Добавьте файлы проекта.

---
+++ RPC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SS13RPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
